3bc2ebe05084cd471ef45e3d45221965d0de1b72
LuoPeiQin <qwer1234A>
2021-10-09 16:10:34 +0800

    传统IO和Okio测试代码上传

--- javalib/src/main/java/com/stark/javalib/net/SocketTest.java
+++ javalib/src/main/java/com/stark/javalib/net/SocketTest.java
@@ -9,12 +9,28 @@ package com.stark.javalib.net;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+
+import okio.Buffer;
+import okio.ByteString;
+import okio.Okio;
+import okio.Sink;
+import okio.Source;
 
 /**
  * @ProjectName: MyPratice
@@ -27,7 +43,91 @@ import java.net.Socket;
 public class SocketTest {
     public static void main(String[] args) {
 //        socketTest();
-        serverSocketTest();
+//        serverSocketTest();
+        // NIO
+//        nioServerSocketTest();
+//        readFile("./javalib/text.txt");
+//        copyFile("./javalib/text.txt", "./javalib/text2.txt");
+        ByteString string = new ByteString(new byte[]{0x10, 0x20, 0x30});
+        System.out.println(string.toString());
+//        okioReadFile("./javalib/text.txt");
+//        okioCopyFile("./javalib/text.txt", "./javalib/text3.txt");
+    }
+
+    private static void okioCopyFile(String srcFilePath, String outFilePath) {
+
+        try (Source source = Okio.source(new File(srcFilePath));
+             Sink sink = Okio.sink(new File(outFilePath));){
+            Buffer buffer = new Buffer();
+            long readLength;
+            while ((readLength = source.read(buffer, 1024)) != -1) {
+                sink.write(buffer, readLength);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void okioReadFile(String filePath) {
+        try (Source source = Okio.source(new File(filePath));){
+            Buffer buffer = new Buffer();
+            while (source.read(buffer, 1024) != -1) {
+                System.out.println(buffer.readString(Charset.defaultCharset()));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void copyFile(String srcFilePath, String outFilePath) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(srcFilePath)));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFilePath)))){
+            String read;
+            while ((read = reader.readLine()) != null) {
+                writer.write(read);
+                writer.write("\n");
+            }
+            writer.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void readFile(String filePath) {
+        System.out.println("开始读取文件" + filePath);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)))){
+            String content;
+            while ((content = reader.readLine()) != null) {
+                System.out.println(content);
+            }
+            System.out.println("文件读取完成" + filePath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void nioServerSocketTest() {
+        try {
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.bind(new InetSocketAddress(80));
+            SocketChannel socketChannel = serverSocketChannel.accept();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            socketChannel.read(byteBuffer);
+            byteBuffer.flip();
+            System.out.println(Charset.defaultCharset().decode(byteBuffer));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void socketTest() {
@@ -56,14 +156,13 @@ public class SocketTest {
             writer.write("HTTP/1.1 200 OK\n" +
                     "Date: Sat, 31 Dec 2021 23:59:59 GMT\n" +
                     "Content-Type: text/html;charset=ISO-8859-1\n" +
-                    "Content-Length: 122\n" +
+                    "Content-Length: 98\n" +
                     "\n" +
                     "＜html＞\n" +
                     "＜head＞\n" +
                     "＜title＞Wrox Homepage＜/title＞\n" +
                     "＜/head＞\n" +
                     "＜body＞\n" +
-                    "＜!-- body goes here --＞\n" +
                     "＜/body＞\n" +
                     "＜/html＞\n\n");
             writer.flush();
