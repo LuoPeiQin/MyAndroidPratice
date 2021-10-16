a321e481dfe0899d0fc334712e18263748dc0cc7
LuoPeiQin <qwer1234A>
2021-10-16 14:50:29 +0800

    用java代码在buildSrc文件中自定义一个DemoPlugin和DemoExtension

---
+++ buildSrc/src/main/java/com/stark/plugins/DemoPlugin.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C), 2007-2021, 未来穿戴有限公司
+ * FileName: DemoPlugin
+ * Author: lyl
+ * Date: 2021/10/16 14:19
+ * Description: 用一句话描述下
+ */
+package com.stark.plugins;
+
+import org.gradle.api.Action;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * @ProjectName: MyPratice
+ * @Package: com.stark.plugins
+ * @ClassName: DemoPlugin
+ * @Description: 用一句话描述下
+ * @Author: lpq
+ * @CreateDate: 2021/10/16 14:19
+ */
+public class DemoPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        System.out.println("apply 执行1");
+        final DemoExtension extension = project.getExtensions().create("inputName", DemoExtension.class);
+        System.out.println("apply 执行2");
+        project.afterEvaluate(new Action<Project>() {
+            @Override
+            public void execute(Project project) {
+                System.out.println("apply 执行3");
+                System.out.println("Hello " + extension.myName);
+            }
+        });
+        System.out.println("apply 执行4");
+
+    }
+}
