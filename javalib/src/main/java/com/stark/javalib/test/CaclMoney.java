e0dc635762df0c9973251f82e1a00c1f2dfc679b
LuoPeiQin <luopeiqin@skg.com>
2022-07-04 17:51:49 +0800

    修改（投资房产和股票资产计算）

--- javalib/src/main/java/com/stark/javalib/test/CaclMoney.java
+++ javalib/src/main/java/com/stark/javalib/test/CaclMoney.java
@@ -8,7 +8,6 @@
 package com.stark.javalib.test;
 
 /**
- *
  * @ProjectName: MyPratice
  * @Package: com.stark.javalib.test
  * @ClassName: CaclMoney
@@ -18,13 +17,52 @@ package com.stark.javalib.test;
  */
 public class CaclMoney {
     public static void main(String[] args) {
-        int everyYear = 20;
-        double everyYearRate = 1.2;
-        double totalMoney = 50;
-        int caclYears = 13;
+        CaclMoney caclMoney = new CaclMoney();
+        caclMoney.caclInvestMoney(10);
+        caclMoney.caclBuyHomeMoney(10);
+//        double baseRate = 1.1;
+//        double total = 1;
+//        for (int i = 0; i < 10; i++) {
+//            total = total * baseRate;
+//            System.out.println("total = " + total);
+//        }
+    }
+
+    public void caclInvestMoney(int caclYears) {
+        System.out.println("投资资产计算");
+        int everyYear = 30;
+        double everyYearRate = 1.25;
+        double totalMoney = 60;
         for (int i = 0; i < caclYears; ++i) {
             totalMoney = (totalMoney * everyYearRate) + everyYear;
-            System.out.println("第" + i + "年的钱数：" + totalMoney);
+//            if (everyYear <= 50) {
+//                everyYear += 10;
+//            }
+            System.out.println("第" + (i + 2022) + "年底的钱数：" + totalMoney);
         }
+    }
+
+    public void caclBuyHomeMoney(int caclYears) {
+        System.out.println("买房资产计算");
+        double homeMoney = 400;
+        double everyYearRate = 1.25;
+        double totalMoney = 60;
+        int everyYear = 30;
+        double everyMonthMoney = 1.4;
+        double firstMoney = 120;
+        totalMoney = totalMoney - firstMoney;
+        for (int i = 0; i < caclYears; ++i) {
+            double everyYearLave = everyYear - (everyMonthMoney * 12);
+            totalMoney += everyYearLave;
+            if (totalMoney > 0) {
+                totalMoney = totalMoney * everyYearRate;
+            }
+//            if (everyYear <= 50) {
+//                everyYear += 10;
+//            }
+            homeMoney = homeMoney * 1.05;
+            System.out.println("第" + (i + 2022) + "年底的钱数：" + totalMoney + "  房产净价值： " + (homeMoney - 400 + 120));
+        }
+
     }
 }
